319b9bb26cafc5e1d02cf83c3ba886acc99fddf8
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-03-26 15:55:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,16 +2,28 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get "/" do
+    @user = "Ian"
+    erb :index #@user will be defined as "Ian" in the view
+  end
+
+  get "/profile" do
+    erb :profile # @user will be nil here
+  end
+  
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+    string = params["string"]
+    @reversed = string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
 
 end
