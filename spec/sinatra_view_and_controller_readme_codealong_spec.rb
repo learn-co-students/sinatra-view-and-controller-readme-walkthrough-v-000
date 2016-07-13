15792521f5d2ea92f6724a54fc394e4a207dc6bd
Han Lee <hanlee12@gmail.com>
2016-07-13 15:31:32 +0000

    done

--- spec/sinatra_view_and_controller_readme_codealong_spec.rb
+++ spec/sinatra_view_and_controller_readme_codealong_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 describe "App" do
 
   describe 'POST /reverse' do
-    
-    it 'responds with a 200' do 
+
+    it 'responds with a 200' do
       params = {
         :string => "Super Silly String"
       }
       post '/reverse', params
       expect(last_response.status).to eq(200)
     end
-    
-    it 'displays the reversed string' do 
+
+    it 'displays the reversed string' do
       params = {
         :string => "Super Silly String"
       }
@@ -23,14 +23,14 @@ describe "App" do
 
   end
 
-  describe 'GET /friends' do 
+  describe 'GET /friends' do
 
-    it 'responds with a 200' do 
+    it 'responds with a 200' do
       get '/friends'
       expect(last_response.status).to eq(200)
     end
 
-    it 'displays friends in the view' do 
+    it 'displays friends in the view' do
       @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
       get '/friends'
       expect(last_response.body).to include("Charlie Chaplin")
@@ -39,6 +39,3 @@ describe "App" do
   end
 
 end
-
-       
-
