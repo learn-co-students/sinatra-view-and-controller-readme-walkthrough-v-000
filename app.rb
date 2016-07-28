77ba0280480e923fd274c358f9443308065b06eb
arturusfury <bri.reynolds@gmail.com>
2016-07-27 23:16:13 -0500

    Done.

--- app.rb
+++ app.rb
@@ -11,7 +11,8 @@ class App < Sinatra::Base
   end
 
   get '/friends' do
-    #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
 
 end
