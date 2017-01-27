941b90f03e5aa4b55f7908b873a5e94e74f0b7ac
Bryna Dunnells <badunnells@gmail.com>
2017-01-27 22:24:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,11 +7,14 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    #write your code here
+  @friends = ["Annie", "Susie","Andrew","Bob","Wendell","Charlie Chaplin","Hobbit","Lola","Eric","Morgan","Brandon","Kari"]
+  erb :friends #write your code here
   end
 
 end
