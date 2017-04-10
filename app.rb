81a95cc3044e65c58ea373727d4468b1167ac351
Annette  Drapalski <annette.drapalski@gmail.com>
2017-04-10 03:17:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-require_relative 'config/environment'
 
 class App < Sinatra::Base
 
@@ -7,11 +6,14 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    original_string = params[:string]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
 
 end
