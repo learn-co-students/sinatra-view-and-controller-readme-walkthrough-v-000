49b3afb2356422d5cd05d7db6ed5f64bf9d4a70f
charlie lee <ctlee9096@gmail.com>
2017-07-09 18:40:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
-    erb :reversed
+  binding.pry
+  @reversed_string = params[:string].reverse
+  @reversed_string
   end
 
   get '/friends' do
