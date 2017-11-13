13a517464055af66004b4d9d8566cc18ae8c4cc8
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-13 15:22:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
 
   get '/friends' do
     # Write your code here! This one uses an array.
-    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    @friends = ['Emily Wilding Davison', 'wiggles', 'Leo', 'Astrid', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
     erb :friends
   end
