16005910cba57d9d74cdadb40202ffb1d6e24e67
Alex <github email address>
2017-12-04 10:25:49 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,12 +5,16 @@ class App < Sinatra::Base
     erb :reverse
   end
 
-  post '/reverse' do
-    erb :reversed
+  post '/reverse' do #receives the params hash from the form
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    erb :reversed   #renders the reversed page
   end
 
   get '/friends' do
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
+    erb :friends
   end
 end
