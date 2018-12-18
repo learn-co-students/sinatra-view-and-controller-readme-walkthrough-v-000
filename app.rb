dbb191e483da01bca7f300c3e78cbcae40a08235
Mohammed Noori <kmanoorib@gmail.com>
2018-12-18 22:46:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,19 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    # i used line 10 in reversed which can get the job done alone, without line 11 and 12
+    # <%= params[:string].reverse %>
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    
+    erb :friends
   end
+  
 end
