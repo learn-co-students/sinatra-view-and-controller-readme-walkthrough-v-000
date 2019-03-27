ae2c4ab29a5e01054837cd7f16d55888cc46112a
Sheena Bonilla <sheena.myb@gmail.com>
2019-03-27 17:01:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do #receives params hash from the form (but does nothing with it) and renders the reversed.erb page
+    original_string = params["string"]
+    @reversed_string = original_string.reverse #creating an instance variable in controller method (route) lets the contents become visible to the erb file to which it renders
     erb :reversed
   end
 
