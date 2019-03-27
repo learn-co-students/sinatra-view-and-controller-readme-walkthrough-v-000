86c6a619548a13ffc47e652adf3226ab0c59fa72
Sheena Bonilla <sheena.myb@gmail.com>
2019-03-27 16:59:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/reverse' do
+  get '/reverse' do #renders the reverse.erb page
     erb :reverse
   end
 
-  post '/reverse' do
+  post '/reverse' do #receives params hash from the form (but does nothing with it) and renders the reversed.erb page
     erb :reversed
   end
 
-  get '/friends' do
+  get '/friends' do #renders the friends.erb page
     # Write your code here!
 
   end
