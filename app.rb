20e5200f01c99ea3ddf7375091cfffbcf478268f
Steven Crouse <sdcrouse91@gmail.com>
2019-06-26 23:53:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -9,8 +9,10 @@ class App < Sinatra::Base
     original_string = params[:string]
     @reversed_string = original_string.reverse
     # puts reversed_string (before changing it to an instance variable)
-    print self.class.ancestors
-    #erb :reversed
+    # print self.class.ancestors
+    #"<%= @reversed_string %>"
+    
+    erb :reversed
   end
 
   get '/friends' do
